9e2879aaad2c12648e3f3cf71c8bab08a4872aa5
dan <dan.lutsch@gmail.com>
2016-01-21 14:24:07 -0800

    adding info to alias.bash file

--- recipes/aliases.rb
+++ recipes/aliases.rb
@@ -1,4 +1,10 @@
-file "/Users/#{node['sprout']['user']}/.bash_it/aliases/available/spruce.aliases.bash"
+cookbook_file "/Users/#{node['sprout']['user']}/.bash_it/aliases/available/spruce.aliases.bash" do
+  source 'spruce.aliases.bash'
+  owner node['sprout']['user']
+  group admin
+  mode '0644'
+  action :create
+end
 
 link "/Users/#{node['sprout']['user']}/.bash_it/aliases/enabled/spruce.aliases.bash" do
   to "/Users/#{node['sprout']['user']}/.bash_it/aliases/available/spruce.aliases.bash"
