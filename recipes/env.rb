755baba6ca1918b6ca697e36485f3038d86257b2
dan <dan.lutsch@gmail.com>
2016-01-21 17:24:26 -0800

    added template for local_profile, prepopulated some values

--- recipes/env.rb
+++ recipes/env.rb
@@ -5,3 +5,15 @@ cookbook_file "/Users/#{node['sprout']['user']}/.bash_it/custom/ra_env_vars.bash
   mode '0644'
   action :create
 end
+
+template "/Users/#{node['sprout']['user']}/.local_profile" do
+  source 'local_profile.erb'
+  owner node['sprout']['user']
+  group 'admin'
+  mode '0644'
+  action :create
+  variables({
+     :local_user => node['sprout']['user']
+  })
+  aciton :create_if_missing
+end
