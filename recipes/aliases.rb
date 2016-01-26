8e19588c67b4fb8b40ab19ef0b0fe6f606ea1900
dan <dan.lutsch@gmail.com>
2016-01-26 10:30:12 -0800

    removing symlink, changing to attribute read by sprout-base

--- recipes/aliases.rb
+++ recipes/aliases.rb
@@ -9,7 +9,7 @@ template "/Users/#{node['sprout']['user']}/.bash_it/aliases/available/spruce.ali
   action :create
 end
 
-link "/Users/#{node['sprout']['user']}/.bash_it/aliases/enabled/spruce.aliases.bash" do
-  to "/Users/#{node['sprout']['user']}/.bash_it/aliases/available/spruce.aliases.bash"
-end
+#link "/Users/#{node['sprout']['user']}/.bash_it/aliases/enabled/spruce.aliases.bash" do
+#  to "/Users/#{node['sprout']['user']}/.bash_it/aliases/available/spruce.aliases.bash"
+#end
 
